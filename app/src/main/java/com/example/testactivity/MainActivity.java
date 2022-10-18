f7bf7239db1ad5c301af307985f98d3f5cba5764
dmayher <82965852+dmayher@users.noreply.github.com>
2022-10-17 20:44:58 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/testactivity/MainActivity.java
+++ app/src/main/java/com/example/testactivity/MainActivity.java
@@ -16,10 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText userInput_00 = findViewById(R.id.gridInput_00);
         final TextView output = findViewById(R.id.OutputBottom);
-        final Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button button1 = findViewById(R.id.gridColor_00);
+        int button1ColorVal = 0;
+        final Button button2 = findViewById(R.id.gridColor_01);
+        final Button button3 = findViewById(R.id.gridColor_02);
+        final Button button4 = findViewById(R.id.gridColor_03);
+        final Button button5 = findViewById(R.id.gridColor_04);
+
+
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View v) {
-                output.setText(userInput_00.getText());
+                //this.turn = 2;
             }
         });
     }
