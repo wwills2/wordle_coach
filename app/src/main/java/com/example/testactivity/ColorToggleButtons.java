9562b18c29d711c8802b1225bedf8127be16593e
Zan Wills <wwills2@umbc.edu>
2022-11-22 17:47:48 -0500

    minor edits

--- app/src/main/java/com/example/testactivity/ColorToggleButtons.java
+++ app/src/main/java/com/example/testactivity/ColorToggleButtons.java
@@ -1,7 +1,5 @@
 package com.example.testactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MyAcivity extends MainActivity {
+public class ColorToggleButtons extends MainActivity {
     public int toggle1, toggle2, toggle3, toggle4, toggle5; // these are button toggles for changing button colors
 }
