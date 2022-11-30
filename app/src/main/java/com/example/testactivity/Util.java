a6eefcf10148830f084edb51f1c511d055447b6d
James Kirk <79110533+Jkirk2@users.noreply.github.com>
2022-11-29 21:29:22 -0500

    Update Util.java

--- app/src/main/java/com/example/testactivity/Util.java
+++ app/src/main/java/com/example/testactivity/Util.java
@@ -1,4 +1,27 @@
 package com.example.testactivity;
 
 public interface Util {
+	static ArrayList<String> readFile(String filepath) {
+		try {
+			BufferedReader bufReader = new BufferedReader(new FileReader(filepath));
+			ArrayList<String> listOfLines = new ArrayList<>();
+
+			String line = null;
+
+			line = bufReader.readLine();
+
+			while (line != null) {
+				listOfLines.add(line);
+				line = bufReader.readLine();
+			}
+
+			bufReader.close();
+			return listOfLines;
+		} catch (IOException e) {
+
+			return null;
+
+		}
+
+	}
 }
